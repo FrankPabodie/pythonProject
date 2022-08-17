da98272eca42f99e5cd217b7d82f329d6129da11
Vadym.Kosin <Vadym.Kosin@tu-dortmund.de>
2022-08-17 16:00:47 +0200

    fixed a bug where the mouse clicked to often, cleaned Code

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-import sys
+import time
 
 import keyboard
-import pynput
 import pytesseract
 import numpy as np
 from PIL import ImageGrab
 import pyautogui
 import cv2 as cv2
 from pynput import keyboard
-from pynput.keyboard import Key,Controller
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
 global stop
+
+
 def on_press(key):
-        if("k" == key.char):
-            global stop
-            stop = True
-            print("stop")
-            exit()
+    if ("k" == key.char):
+        global stop
+        stop = True
+        print("stop")
         print('alphanumeric key {0} pressed'.format(
             key.char))
+
 
 def on_release(key):
     print('{0} released'.format(
         key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
 
 
 def proccesImg(img):
@@ -37,8 +33,6 @@ def proccesImg(img):
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     txt = pytesseract.image_to_string(img).split("\n")
     del txt[-1]
-    print(txt)
-    cv2.imshow("Result", img)
     return txt
 
 
@@ -58,22 +52,21 @@ def startsearch():
     listener.start()
 
     while i >= 0:
+        txt = proccesImg(image)
+        print(txt)
         if stop:
             break
 
-        if any("Blood Plague" in n for n in proccesImg(image)):
-            print("swap")
+        if any("Blood Plague" in n for n in txt) or any("Just Keeps Going" in n for n in txt):
+            print("swap" + str(x))
             i = i - 1
             x = x + 613
-            image = ImageGrab.grab(bbox=(x, y, x + offx, y + offy))
-        elif any("Just Keeps Going" in n for n in proccesImg(image)):
-            print("swap")
-            i = i - 1
-            x = x + 613
-            image = ImageGrab.grab(bbox=(x, y, x + offx, y + offy))
-        else:
-            pyautogui.click(x, y + 500)
-            image = ImageGrab.grab(bbox=(x, y, x + offx, y + offy))
+            print(str(x))
+            pyautogui.moveTo(x, y+500)
+            time.sleep(0.5)
+        time.sleep(0.5)
+        pyautogui.click(x=x, y=y + 500)
+        image = ImageGrab.grab(bbox=(x, y, x + offx, y + offy))
 
 
 startsearch()
